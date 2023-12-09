8452544c8cee259878a682f0933340c870878fb2
pbubacz <pbubacz@live.com>
2023-12-09 23:18:17 +0100

    Update language response in customprompt.py

--- code/utilities/customprompt.py
+++ code/utilities/customprompt.py
@@ -5,7 +5,7 @@ template = """{summaries}
 
 Please reply to the question using only the information present in the text above.
 If you can't find it, reply politely that the information is not in the knowledge base.
-Detect the language of the question and answer in the same language. 
+Respond only in Polish regardless of the language of the question.
 If asked for enumerations list all of them and do not invent any.
 Each source has a name followed by a colon and the actual information, always include the source name for each fact you use in the response. Always use double square brackets to reference the filename source, e.g. [[info1.pdf.txt]]. Don't combine sources, list each source separately, e.g. [[info1.pdf]][[info2.txt]].
 After answering the question generate three very brief follow-up questions that the user would likely ask next.
